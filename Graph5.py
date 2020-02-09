d8a8029d04f65a8335eff548455b50c05c7b6743
babiswas <36biswas0814@gmail.com>
2020-02-10 00:08:41 +0530

    added

---
+++ Graph5.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self,vertex):
+        self.vertex=vertex
+        self.graph=defaultdict(list)
+
+    def add_edges(self,u,v):
+        self.graph[u].append(v)
+
+    def topsort_util(self,visited,stack,u):
+        visited[u]=True
+        for i in self.graph[u]:
+          if not visited[i]:
+             self.topsort_util(visited,stack,i)
+        stack.append(u)
+
+    def top_sort(self):
+        stack=[]
+        visited=[False]*self.vertex
+        for i in range(self.vertex):
+          if not visited[i]:
+             self.topsort_util(visited,stack,i)
+        while stack:
+           print(stack.pop())
+
+if __name__=="__main__":
+   graph=Graph(6)
+   graph.add_edges(5,0)
+   graph.add_edges(4,0)
+   graph.add_edges(5,2)
+   graph.add_edges(2,3)
+   graph.add_edges(4,1)
+   graph.add_edges(3,1)
+   graph.top_sort()
+
